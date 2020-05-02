ae32e4a61549f7ef9781c3eed2e2cb77c66c9a1a
Adan Pelcastre Roblero <znup.kreyz.vlc@gmail.com>
2020-05-02 09:45:43 -0700

    actualizacion settings

--- AdanCv/settings.py
+++ AdanCv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^tf2r!d2mbekfp2p735h81joz)=t+-cv!40ypvb#jv$=-u#iy@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
